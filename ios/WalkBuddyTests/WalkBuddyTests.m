dc5a6d76f2acc7d872d170042665362923589f7b
Yunus Emre EKE <yemreeke1@gmail.com>
2024-01-31 22:16:35 +0300

    project name renamed

--- ios/WalkBuddyTests/WalkBuddyTests.m
+++ ios/WalkBuddyTests/WalkBuddyTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface TemplateTests : XCTestCase
+@interface WalkBuddyTests : XCTestCase
 
 @end
 
-@implementation TemplateTests
+@implementation WalkBuddyTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
